08863bc32122cf4a2ebf820b9f624b7c5606ca7b
Dlxxvii <a@a.ru>
2021-03-03 14:20:55 +0300

    Make playing field

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Button[] Cells;
-    // Start is called before the first frame update
+    public Sprite Tac;
+    public Sprite Toe;
+
+    private bool _flag = true;
+    private int[] _flagsInts = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        // Start is called before the first frame update
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
         
     }
 
-    public void OnClick()
+    public  void OnClick(int i)
     {
-        print("as");
+        switch (_flag)
+        {
+            case true:
+                if (_flagsInts[i - 1] != 1)
+                {
+                    Cells[i - 1].image.sprite = Toe;
+                    _flag = false;
+                    _flagsInts[i - 1] = 1;
+                }
+                break;
+            case false:
+                if (_flagsInts[i - 1] != 1)
+                {
+                    Cells[i - 1].image.sprite = Tac;
+                    _flag = true;
+                    _flagsInts[i - 1] = 1;
+                }
+                break;
+        }
+
     }
 }
     

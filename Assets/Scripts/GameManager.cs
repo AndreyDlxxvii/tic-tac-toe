03f9acf4480c8fb82076bb93e26a6d889d84764c
Dlxxvii <a@a.ru>
2021-03-05 14:11:42 +0300

    Check win

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,7 +12,9 @@ public class GameManager : MonoBehaviour
     public Sprite Toe;
 
     private bool _flag = true;
-    private int[] _flagsInts = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private int[] _flagsIntsTac = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private int[] _flagsIntsToe = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private int[] _check = new int[9] {1, 1, 1, 0, 0, 0, 0, 0, 0};
 
         // Start is called before the first frame update
     void Start()
@@ -30,25 +33,27 @@ public class GameManager : MonoBehaviour
         switch (_flag)
         {
             case true:
-                if (_flagsInts[i - 1] == 0)
+                if (_flagsIntsTac[i - 1] == 0 && _flagsIntsToe[i-1] == 0)
                 {
                     Cells[i - 1].image.sprite = Tac;
                     _flag = false;
-                    _flagsInts[i - 1] = 1;
+                    _flagsIntsTac[i - 1] = 1;
                 }
                 break;
             case false:
-                if (_flagsInts[i - 1] == 0)
+                if (_flagsIntsTac[i - 1] == 0 && _flagsIntsToe[i-1] == 0)
                 {
                     Cells[i - 1].image.sprite = Toe;
                     _flag = true;
-                    _flagsInts[i - 1] = 2;
+                    _flagsIntsToe[i - 1] = 2;
                 }
                 break;
         }
-
-
-
+       if (_check.SequenceEqual(_flagsIntsTac))
+        {
+            
+            print("it works!");
+        }
     }
 }
     

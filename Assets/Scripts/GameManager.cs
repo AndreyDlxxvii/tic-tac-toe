7964a85b2a69135636ff7d53441d235e86a38a05
Dlxxvii <a@a.ru>
2021-03-05 15:15:24 +0300

    change

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,11 +15,20 @@ public class GameManager : MonoBehaviour
     private int[] _flagsIntsTac = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
     private int[] _flagsIntsToe = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
     private int[] _check = new int[9] {1, 1, 1, 0, 0, 0, 0, 0, 0};
+    private int[] _check1 = new int[9] {1, 0, 0, 1, 0, 0, 1, 0, 0};
+    private int[] _check2 = new int[9] {0, 0, 0, 0, 0, 0, 1, 1, 1};
+    private int[] _check3 = new int[9] {1, 0, 0, 0, 1, 0, 0, 0, 1};
+    private int[] _check4 = new int[9] {0, 1, 0, 0, 1, 0, 0, 1, 0};
+    private int[] _check5 = new int[9] {0, 0, 1, 0, 1, 0, 1, 0, 0};
+    private int[] _check6 = new int[9] {0, 0, 0, 1, 1, 1, 0, 0, 0};
+    private int[] _check7 = new int[9] {0, 0, 1, 0, 0, 1, 0, 0, 1};
+
+    private List<int[]> _checks = new List<int[]>();
 
         // Start is called before the first frame update
     void Start()
     {
-        
+        _checks[0] = _check;
     }
 
     // Update is called once per frame
@@ -45,11 +54,11 @@ public class GameManager : MonoBehaviour
                 {
                     Cells[i - 1].image.sprite = Toe;
                     _flag = true;
-                    _flagsIntsToe[i - 1] = 2;
+                    _flagsIntsToe[i - 1] = 1;
                 }
                 break;
         }
-       if (_check.SequenceEqual(_flagsIntsTac))
+       if (_checks[0].SequenceEqual(_flagsIntsTac))
         {
             
             print("it works!");

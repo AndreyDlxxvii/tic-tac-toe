9a737fdd2f1cbc0a85e805310dd0d69c82b40d77
Dlxxvii <a@a.ru>
2021-03-09 15:53:27 +0300

    Add restart button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -23,46 +25,70 @@ public class GameManager : MonoBehaviour
     private int[] _check6 = new int[9] {0, 0, 0, 1, 1, 1, 0, 0, 0};
     private int[] _check7 = new int[9] {0, 0, 1, 0, 0, 1, 0, 0, 1};
 
-    private List<int[]> _checks = new List<int[]>();
+    private List<int[]> _checksList = new List<int[]>();
 
-        // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        _checks[0] = _check;
+        _checksList.Add(_check);
+        _checksList.Add(_check1);
+        _checksList.Add(_check2);
+        _checksList.Add(_check3);
+        _checksList.Add(_check4);
+        _checksList.Add(_check5);
+        _checksList.Add(_check6);
+        _checksList.Add(_check7);
     }
 
     // Update is called once per frame
-    void Update() 
+    void Update()
     {
-        
+
     }
 
-    public  void OnClick(int i)
+    public void OnClick(int i)
     {
         switch (_flag)
         {
             case true:
-                if (_flagsIntsTac[i - 1] == 0 && _flagsIntsToe[i-1] == 0)
+                if (_flagsIntsTac[i - 1] == 0 && _flagsIntsToe[i - 1] == 0)
                 {
                     Cells[i - 1].image.sprite = Tac;
                     _flag = false;
                     _flagsIntsTac[i - 1] = 1;
                 }
+
                 break;
             case false:
-                if (_flagsIntsTac[i - 1] == 0 && _flagsIntsToe[i-1] == 0)
+                if (_flagsIntsTac[i - 1] == 0 && _flagsIntsToe[i - 1] == 0)
                 {
                     Cells[i - 1].image.sprite = Toe;
                     _flag = true;
                     _flagsIntsToe[i - 1] = 1;
                 }
+
                 break;
         }
-       if (_checks[0].SequenceEqual(_flagsIntsTac))
+
+        for (int j = 0; j < 8; j++)
         {
-            
-            print("it works!");
+            if (_checksList[j].SequenceEqual(_flagsIntsTac))
+            {
+                print("it works, tac win!");
+            }
+
+            if (_checksList[j].SequenceEqual(_flagsIntsToe))
+            {
+                print("it works toe win!");
+            }
         }
+
     }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
     

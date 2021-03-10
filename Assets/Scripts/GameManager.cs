5d33bfdf9d44d3c3338222d71f2d930b0f759228
Dlxxvii <a@a.ru>
2021-03-10 15:11:24 +0300

    CheckToWin do it

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,25 +17,25 @@ public class GameManager : MonoBehaviour
     private int _countCross = 0;
     private int _countZero = 0;
     private bool _checkGameOver = true;
-    private int[] _flagsIntsCross = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    private int[] _flagsIntsZero = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    private int[] _check = new int[9] {1, 1, 1, 0, 0, 0, 0, 0, 0};
-    private int[] _check1 = new int[9] {1, 0, 0, 1, 0, 0, 1, 0, 0};
-    private int[] _check2 = new int[9] {0, 0, 0, 0, 0, 0, 1, 1, 1};
-    private int[] _check3 = new int[9] {1, 0, 0, 0, 1, 0, 0, 0, 1};
-    private int[] _check4 = new int[9] {0, 1, 0, 0, 1, 0, 0, 1, 0};
-    private int[] _check5 = new int[9] {0, 0, 1, 0, 1, 0, 1, 0, 0};
-    private int[] _check6 = new int[9] {0, 0, 0, 1, 1, 1, 0, 0, 0};
-    private int[] _check7 = new int[9] {0, 0, 1, 0, 0, 1, 0, 0, 1};
+    private readonly int[] _flagsIntsCross = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private readonly int[] _flagsIntsZero = new int [9] {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private readonly int[] _check = new int[9] {1, 1, 1, 0, 0, 0, 0, 0, 0};
+    private readonly int[] _check1 = new int[9] {1, 0, 0, 1, 0, 0, 1, 0, 0};
+    private readonly int[] _check2 = new int[9] {0, 0, 0, 0, 0, 0, 1, 1, 1};
+    private readonly int[] _check3 = new int[9] {1, 0, 0, 0, 1, 0, 0, 0, 1};
+    private readonly int[] _check4 = new int[9] {0, 1, 0, 0, 1, 0, 0, 1, 0};
+    private readonly int[] _check5 = new int[9] {0, 0, 1, 0, 1, 0, 1, 0, 0};
+    private readonly int[] _check6 = new int[9] {0, 0, 0, 1, 1, 1, 0, 0, 0};
+    private readonly int[] _check7 = new int[9] {0, 0, 1, 0, 0, 1, 0, 0, 1};
 
-    private List<int[]> _checksList = new List<int[]>();
+    private readonly List<int[]> _checksList = new List<int[]>();
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < Cells.Length; i++)
+        foreach (var t in Cells)
         {
-            Cells[i].gameObject.SetActive(true);
+            t.gameObject.SetActive(true);
         }
         _checksList.Add(_check);
         _checksList.Add(_check1);
@@ -66,7 +66,6 @@ public class GameManager : MonoBehaviour
                         _flag = false;
                         _flagsIntsCross[i - 1] = 1;
                     }
-
                     break;
                 case false:
                     if (_flagsIntsCross[i - 1] == 0 && _flagsIntsZero[i - 1] == 0)
@@ -75,39 +74,44 @@ public class GameManager : MonoBehaviour
                         _flag = true;
                         _flagsIntsZero[i - 1] = 1;
                     }
-
                     break;
             }
-
-            for (int j = 0; j < 8; j++)
-            {
-                for (int k = 0; k < 9; k++)
-                {
-                    if (_checksList[k]==_flagsIntsCross)
-                    {
-                        _countCross++;
-                    }
-                    else if (_checksList[k] == _flagsIntsZero)
-                    {
-                        _countZero++;
-                    }
-                    if (_countCross==3)
-                    {
-                        print("it works, Cross win!");
-                        _checkGameOver = false;
-                    }
-                    
-                    if (_countZero==3)
-                    {
-                        print("it works Zero win!");
-                        _checkGameOver = false;
-                    }
-                }
-                
-            }
+            CheckToWin();
         }
-        
+    }
 
+    private void CheckToWin()
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            _countCross = 0;
+            _countZero = 0;
+            for (int k = 0; k < 9; k++)
+            {
+                if (_checksList[j][k]==_flagsIntsCross[k] && _checksList[j][k]==1)
+                {
+                    _countCross++;
+                }
+                if (_checksList[j][k]==_flagsIntsZero[k] && _checksList[j][k]==1)
+                {
+                    _countZero++;
+                }
+                if (_countCross==3)
+                {
+                    print("it works, Cross win!");
+                    _checkGameOver = false;
+                    break;
+                }
+                    
+                if (_countZero==3)
+                {
+                    print("it works Zero win!");
+                    _checkGameOver = false;
+                    break;
+                }
+            }
+                
+        }
     }
 
     public void RestartGame()
